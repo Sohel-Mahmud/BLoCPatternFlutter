b56d2c8f96f2aaf4b03ea8c319ef2ddd041ad866
sohel-mahmud <sohel7avon@gmail.com>
2023-04-05 16:25:17 +0600

    gg

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -16,17 +16,21 @@ import 'package:blocpattern/timer/bloc/timer_state.dart';
 import 'package:blocpattern/timer/ticker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class TimerBloc extends Bloc<TimerEvent, TimerState>{
-
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
   final Ticker _ticker;
 
   static const int _duration = 60;
 
   StreamSubscription<int>? _tickerSubscription;
-  
-  TimerBloc({required Ticker ticker}) : _ticker = ticker,
-  super(TimerInitial(_duration)){
-    
+
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(const TimerInitial(_duration)) {
+
+    on<TimerStarted>(
+      (event, emit) {
+
+      }
+    );
   }
-  
 }
